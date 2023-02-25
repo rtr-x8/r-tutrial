8a6bbf256e5bbaa487a895ed73878263556546cd
rtr-x8 <ryu.2.24@hotmail.co.jp>
2023-02-25 23:07:06 +0900

    first commit

---
+++ practice.R
@@ -0,0 +1,19 @@
+# R入門
+z = 3 # zに3を代入する
+pi # 円周率
+print(pi) # 変数で呼び出すのは勝手にprintに渡される
+print(pi, digits = 22) # 桁数を渡せば丸桁が変わるが、保持している桁数に依存する
+sqrt(3) #平方根 square root
+2**3
+2^3 # 
+exp(10) # 指数関数 expotential 
+exp(1) #自然対数 ネイピア数
+log(2) # 対数関数
+log(10) # 自然対数を底とする
+log10(10)
+log2(10)
+sin(pi/2) #sin
+cos(2/3)
+acos(1/2)
+sinpi(2/3) # sinpi(x) = sin(x*pi)
+acos(1/2) #逆三角関数
